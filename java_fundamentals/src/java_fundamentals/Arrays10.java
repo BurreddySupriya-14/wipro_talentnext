38cab0cf22f96b8eeb08dbf5b9519c8c181189a6
Burreddy Supriya <supriyareddy1462@gmail.com>
2025-07-08 15:05:33 +0530

    java_fundamentals

--- java_fundamentals/src/java_fundamentals/Arrays10.java
+++ java_fundamentals/src/java_fundamentals/Arrays10.java
@@ -1,6 +1,7 @@
+package java_fundamentals;
 import java.util.Arrays;
 
-public class Main {
+public class Arrays10 {
     public static void main(String[] args) {
         int[] arr = {1, 0, 1, 0, 0, 1, 1}; 
         int[] result = new int[arr.length];
