42989efd1cc77e85bbd1e2179bf64a5574ccae91
Burreddy Supriya <supriyareddy1462@gmail.com>
2025-07-11 20:59:12 +0530

    oops

---
+++ oops/src/oops/Class_object2.java
@@ -0,0 +1,17 @@
+package oops;
+class Calculator{
+	public static int powerInt(int num1,int num2) {
+		return (int) Math.pow(num1 , num2);
+	}
+	public static double powerDouble(double num1, double num2) {
+		return Math.pow(num1,num2);
+	}
+}
+public class Class_object2{
+	public static void main(String[]args) {
+		int intResult=Calculator.powerInt(2, 3);
+		double doubleResult=Calculator.powerDouble(2.5, 3);
+		System.out.println("IntResult Value: "+intResult);
+		System.out.println("DoubleResult Value: "+doubleResult);
+	}
+}
